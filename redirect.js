8582fa85024be2f3f12d1e9f2b985d7e98adc59f
Tim Saucer <timothy.saucer@soartech.com>
2020-12-01 23:04:38 -0500

    Switch over to use an icon you can click on to swap between AADL and direct

--- redirect.js
+++ redirect.js
@@ -1,1 +1,13 @@
-window.location.href = "https://www-nytimes-com.research.aadl.org" + window.location.pathname;
+function redirect(tab) {
+    var prior_url = tab.url;
+    if (prior_url.includes("www.nytimes.com")) {
+        var updated_url = prior_url.replace("www.nytimes.com", "www-nytimes-com.research.aadl.org");
+        browser.tabs.update(tab.id, {url: updated_url});
+    }
+    else if (prior_url.includes("www-nytimes-com.research.aadl.org")) {
+        var updated_url = prior_url.replace("www-nytimes-com.research.aadl.org", "www.nytimes.com");
+        browser.tabs.update(tab.id, {url: updated_url});
+    }
+};
+
+browser.pageAction.onClicked.addListener(redirect);
