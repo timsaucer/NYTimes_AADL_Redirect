1d5a0f92342951c88e75676da5b45c3393346641
Tim Saucer <timothy.saucer@soartech.com>
2020-12-02 08:09:09 -0500

    Chrome needed a slightly different way to make the extension show

--- redirect.js
+++ redirect.js
@@ -1,13 +1,25 @@
+var browser = browser || chrome;
+
 function redirect(tab) {
     var prior_url = tab.url;
     if (prior_url.includes("www.nytimes.com")) {
         var updated_url = prior_url.replace("www.nytimes.com", "www-nytimes-com.research.aadl.org");
-        browser.tabs.update(tab.id, {url: updated_url});
+        chrome.tabs.update(tab.id, {url: updated_url});
     }
     else if (prior_url.includes("www-nytimes-com.research.aadl.org")) {
         var updated_url = prior_url.replace("www-nytimes-com.research.aadl.org", "www.nytimes.com");
-        browser.tabs.update(tab.id, {url: updated_url});
+        chrome.tabs.update(tab.id, {url: updated_url});
     }
 };
 
-browser.pageAction.onClicked.addListener(redirect);
+chrome.pageAction.onClicked.addListener(redirect);
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+    // If the letter 'g' is found in the tab's URL...
+    if (tab.url.includes("www.nytimes.com") || tab.url.includes("www-nytimes-com.research.aadl.org")) {
+        chrome.pageAction.show(tabId);
+    }
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
